9c8835d6e7dafc425dfea966063c8732cd0f2ba7
ASHISH PATEL <ASHISHPATELUK@GMAIL.COM>
2023-10-28 12:26:53 +0100

    THIS IS MY JAVA FILE

---
+++ MyClass11.java
@@ -0,0 +1,25 @@
+package mypackage11;
+
+
+public class MyClass11 {
+
+    public static void main(String[] args){
+
+        System.out.println("What is ghjkhfkk?");
+
+// Manual Topics
+
+        /* fbfbkfkffh
+         hrltlrjlrtj */
+
+//        gggeir
+
+        /*hghgoghgego*/
+
+
+        System.out.println("What is ghjhruririttjtk?") ;
+    }
+}
+
+
+
